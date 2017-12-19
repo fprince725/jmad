a512ed2724e4998fa8f1dfc15f97eb7a64c0e676
Frank Prince <Frank.Prince@cox.net>
2017-12-19 00:29:27 -0600

    Initial functional test scaffolding

--- jmad/tests.py
+++ jmad/tests.py
@@ -2,11 +2,20 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 
 class StudentTestCase(LiveServerTestCase):
-	def setup(self):
+	def setUp(self):
 		self.browser = webdriver.Chrome()
-		self.browser - implicitly_wait(2)
+		self.browser.implicitly_wait(10)
+	
+	def tearDown(self):
+		self.browser.quit()
 
 	def test_student_find_solos(self):
+		home_page = self.browser.get(self.live_server_url + '/')
+		# home_page = self.browser.get('http://www.facebook.com')
+
+		brand_element = self.browser.find_element_by_css_selector('.navbar_brand')
+		self.assertEqual('JMAD', brand_element.text)
+		self.fail('Incomplete Test')
 
 		'''
 		test finding solos
